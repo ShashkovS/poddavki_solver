a9eb4aba80c977ad8191688dc89e04282fb836dd
Саня-ПК\Саня <Donk.Wow>
2017-12-20 20:43:58 +0300

    поле по одному числу для двух шашек(пока без дамок) + таблица с 255 и нулями

--- poddavki_for_2.py
+++ poddavki_for_2.py
@@ -1,15 +1,22 @@
-board = [[0] * 8]* 8
+table_ro_poddavki = [253] * 32 * 33 * 3
 
 
-for pos in range(32 * 33 * 2):
-    pos += 1
-    if pos % 2 == 0:
+for pos in range(32 * 33 * 3):
+    pos_l = pos
+    if pos % 2 == 0 and pos % 3 != 0:
         pos //= 2
-        first_cell = ['W', pos // 32]
+        first_cell = ['W', pos // 32 + 1]
+        second_cell = ['B', pos % 32 + 1]
+    elif pos % 3 == 0:
+        pos = (pos + 1) // 2
+        first_cell = ['B', pos // 32 + 1]
         second_cell = ['B', pos % 32 + 1]
     else:
-        pos = (pos + 1) // 2
-        first_cell = ['W', pos // 32]
+        pos = (pos + 2) // 2
+        first_cell = ['W', pos // 32 + 1]
         second_cell = ['W', pos % 32 + 1]
-    if first_cell[1] != second_cell[1] and first_cell[1] != 33:
+    if first_cell[1] != second_cell[1] and first_cell[1] != 33 and first_cell[1] != 34:
         print(first_cell, second_cell)
+        if first_cell[0] == second_cell[0]:
+            table_ro_poddavki[pos_l] = 0 if first_cell[0] == 'W' else 255
+print(table_ro_poddavki)
