e508bf5fe8434fe6a95ea2479fc2145e596223b9
Артём <artemsh05@mail.ru>
2017-12-22 00:01:26 +0300

    Оно пока не работает

--- poddavki_for_2_n2.py
+++ poddavki_for_2_n2.py
@@ -1,4 +1,6 @@
 av_coor_pos = []
+white_cells = []
+black_cells = []
 for i in range(8):
     for c in range(0 if i % 2 == 1 else 1, 8, 2):
         av_coor_pos.append(str(i) + str(c))
@@ -8,6 +10,9 @@ for pos_1 in range(len(av_coor_pos)):
         for pos_2 in range(pos_1 + 1, len(av_coor_pos)):
             for colour_2 in 'B', 'W':
                 may_pos.append([[colour_1, av_coor_pos[pos_1]], [colour_2, av_coor_pos[pos_2]]])
-print(may_pos)
-# Выводит все варианты позиций для 2 фишек.
-# Каждый вариант записывается в виде списка, в котором есть два списка, в которых [0]=цвет фишки, [1]=координаты фишки
+
+
+def check_need_eat():
+    for wc in white_cells:
+        for i in [1, 1], [1, -1], [-1, 1], [-1, -1]:
+
