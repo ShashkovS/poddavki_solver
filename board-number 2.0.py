3062442fdbf65a5e0abe40bb3088d1d5e5d84bd6
ASUS_ILIAN\Илиан <s22b_andrianov@179.ru>
2017-12-26 21:21:47 +0300

    24 фишки. Номер позиции...

--- board-number 2.0.py
+++ board-number 2.0.py
@@ -1,6 +1,8 @@
-def print_board(board):
+def print_board(board, step):
+    step = 'B' if step == '1' else 'W'
     rep = '    ' + ' '.join(list(map(str, [i for i in range(1, 9)]))) + '\n'
     for i in range(1, 9): rep += '\n' + str(i) + '   ' + ' '.join(list(map(str, board[i - 1])))
+    print('Ход', step)
     print(rep)
 
 
@@ -16,20 +18,24 @@ def count_k_symbols(num, k):
 
 
 def num_to_board(num_board):
-    num_board, trans = count_k_symbols(num_board, 32), str.maketrans('012345', '.BWbw*')
+    num_board, trans = count_k_symbols(num_board, 33), str.maketrans('012345', '.BWbw*')
+    num_board, step = num_board[:-1], num_board[-1]
+    print(num_board)
     board = [['.' for i in range(8)] for j in range(8)]
     for lin in range(0, 8):
         for i, col in enumerate(range((lin + 1) % 2, 8, 2), start=lin * 4):
             board[lin][col] = num_board[i].translate(trans)
-    return board
+    print(board)
+    return board, step
 
 
-def board_to_num(board):
+def board_to_num(board, step='1'):
     num_board, trans = '', str.maketrans('.BWbw*', '012345')
     for lin in range(0, 8):
         for col in range((lin + 1) % 2, 8, 2):
+            print(board)
             num_board += board[lin][col].translate(trans)
-    return num_board
+    return num_board + step
 
 
 ##########################################################################
@@ -41,14 +47,16 @@ def board_to_num(board):
 # 3 - b
 # 4 - w
 # 5 - * (доступные ходы для выбранного цвета фигур)(пока нету такой функции)
+# Последняя цифра - чей ход (1 - BLACK, 2 - WHITE)
+
 
 
 # TEST
 
 while True:
-    n = input()
-    board = num_to_board(n)
+    n = input() + '1'
+    board, step = num_to_board(n)
     num_board = board_to_num(board)
-    print_board(board)
+    print_board(board, step)
     print(num_board)
     print('\n\n')
